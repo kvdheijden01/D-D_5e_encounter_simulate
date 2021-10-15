2ed370495193ab979d76b6712ddecf74534e8359
kvdheijden <kvdheijden@sogyo.nl>
2021-10-15 12:47:03 +0200

    Show individual creature in web app and link in the index

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,5 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 root "creature#index"
 
-  get "/creature", to: "creature#index"
-  get "/creature/:name" to: "creature#show"
-  post "/creature", to: "creature#save_creature"
+  resources :creature
 end
