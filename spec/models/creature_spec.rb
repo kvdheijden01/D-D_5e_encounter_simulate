3257519ad7b0015f8fa75fd8ba04b08bd08f16d4
kvdheijden <kvdheijden@sogyo.nl>
2021-10-15 16:10:13 +0200

    test creature CRUD

--- spec/models/creature_spec.rb
+++ spec/models/creature_spec.rb
@@ -4,17 +4,18 @@ RSpec.describe Creature, type: :model do
   describe "validation tests" do
     let(:creature) { build(:creature) }
 
-    it "tests that factory is valid" do
+    it "factory is valid" do
       creature = build(:creature)
       expect(creature).to be_valid  
     end
-  end
 
-  describe "database tests" do
-    it "tests if creature can be created in database" do
-      goblin = create(:creature)
-      goblin.save
-      expect(Creature.all.length).to eq(1)
+    it "name has to be unique" do
+      creature1 = create(:creature, name: "Goblin")
+      creature2 = build(:creature, name: creature1.name)
+      creature3 = build(:creature, name: "Imp")
+      expect(creature1).to be_valid
+      expect(creature2).not_to be_valid
+      expect(creature3).to be_valid
     end
   end
 end
--- spec/requests/creature_spec.rb
+++ spec/requests/creature_spec.rb
@@ -3,9 +3,20 @@ require 'rails_helper'
 RSpec.describe "Creatures", type: :request do
   describe "GET /index" do
     it "returns http success" do
-      get "/creature/index"
-      expect(response).to have_http_status(:success)
+      get "/creatures"
+      expect(response).to have_http_status(:ok)
     end
+
+    it "displays a created creatures name" do
+      creature = create(:creature)
+      creature2 = create(:creature, name: "Goblin")
+      get '/creatures'
+      expect(response.body).to include(creature.name)
+      expect(response.body).to include(creature2.name)
+    end
+
   end
+
+
 
 end
