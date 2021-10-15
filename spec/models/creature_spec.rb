66e46c7faf192671e82e408e0e36dd8b0d7cb147
kvdheijden <kvdheijden@sogyo.nl>
2021-10-15 11:42:25 +0200

    creature names depict on web page

--- spec/models/creature_spec.rb
+++ spec/models/creature_spec.rb
@@ -1,5 +1,16 @@
 require 'rails_helper'
 
 RSpec.describe Creature, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe "validation tests" do
+    let(:creature) { build(:creature) }
+
+    it "tests that factory is valid" do
+      creature = build(:creature)
+      expect(creature).to be_valid  
+    end
+  end
+
+  describe "database tests" do
+    
+  end
 end
