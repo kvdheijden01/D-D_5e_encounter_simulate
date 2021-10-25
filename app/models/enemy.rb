934eb124c97ca6714111bb45f2670abc750d6c71
kvdheijden <kvdheijden@sogyo.nl>
2021-10-25 11:51:40 +0200

    calculate HP pool

--- app/models/enemy.rb
+++ app/models/enemy.rb
@@ -1,3 +1,12 @@
 class Enemy < ApplicationRecord
-    has_many :creatures
+  has_many :creatures
+
+  def cal_HPpool
+    creatures.each do |creature|
+      puts self.HPpool
+      puts creature.creaturetype.HP
+      self.HPpool = self.HPpool + creature.creaturetype.HP
+      puts self.HPpool
+    end
+  end
 end
--- spec/models/enemy_spec.rb
+++ spec/models/enemy_spec.rb
@@ -1,8 +1,9 @@
+# frozen_string_literal: true
 require 'rails_helper'
 
 RSpec.describe Enemy, type: :model do
-  describe "validation" do
-    it "enemy - creature model relations can retrieve name" do
+  describe 'validation' do
+    it 'enemy - creature model relations can retrieve name' do
       creaturetype = create(:creaturetype)
       creature = create(:creature, creaturetype: creaturetype)
       enemy = create(:enemy)
@@ -10,6 +11,25 @@ RSpec.describe Enemy, type: :model do
       expect(enemy.creatures.first.creaturetype.name).to eq(creaturetype.name)
     end
 
+  end
+
+  describe 'calcualtions' do
+    it 'tests if HPpool is calculated' do
+      enemy = enemy_setup('Goblin', 'Imp')
+      expect(enemy.HPpool).to eq(0)
+      enemy.cal_HPpool
+      expect(enemy.HPpool).to eq(2)
+    end
+
+  end
+
+  def enemy_setup(*names)
+    enemy = create(:enemy)
+    names.each do |name|
+      creaturetype = create(:creaturetype, name: name)
+      enemy.creatures.push(create(:creature, creaturetype: creaturetype))
+    end
+    enemy
   end
 
 end
